ba98c88166599bcd2b287d14f52e576c290d1073
Chesvic Lordgape <lordgape@gmail.com>
2020-12-28 22:07:05 +0100

    Update test

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 "use strict";
 
-const permutation = (str) => {
-  permutateWord(str, 0, str.length - 1);
-};
-
-const permutateWord = (str, startingIndex, endingIndex) => {
-  if (startingIndex == endingIndex) console.log(str);
-
-  for (let i = startingIndex; i <= endingIndex; i++) {
-    str = swap(str, startingIndex, i);
-    permutateWord(str, startingIndex + 1, endingIndex);
-    str = swap(str, startingIndex, i);
-  }
-};
-
-const swap = (str, fromPosition, toPosition) => {
-  let stringArray = str.split("");
-
-  let temp = stringArray[fromPosition];
-  stringArray[fromPosition] = stringArray[toPosition];
-  stringArray[toPosition] = temp;
-
-  return stringArray.join("");
 
 
-  
-};
 
-console.log("", permutation("TURING"));
